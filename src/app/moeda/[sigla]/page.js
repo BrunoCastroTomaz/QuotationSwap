7803b1f55b1eccb1b6009a3df032206ff06b6c3d
rafaelapz <rafaela@hotmail.com>
2025-05-21 16:02:16 -0300

    deixei nova tela mais bonita

--- src/app/moeda/[sigla]/page.js
+++ src/app/moeda/[sigla]/page.js
@@ -2,26 +2,26 @@
 
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
+import './moeda.css';
 
 export default function MoedaPage() {
   const params = useParams();
   const sigla = params.sigla;
   
   return (
-    <div style={{padding: '20px', textAlign: 'center'}}>
-      <h1>Página da moeda: {sigla}</h1>
-      
-      <Link href="/" style={{
-        display: 'inline-block',
-        padding: '10px 20px',
-        backgroundColor: '#007bff',
-        color: 'white',
-        textDecoration: 'none',
-        borderRadius: '5px',
-        marginTop: '20px'
-      }}>
-        Voltar para Home
-      </Link>
+    <div className="moeda-container">
+      <h1>MOEDINHAS</h1> 
+      <h2>0,0 BRL</h2>
+      <input className="input-display" type="number" placeholder={`0,00 ${sigla}`}/> 
+
+      <div className="link-container">
+        <Link href="/" className="back-button">
+        ←
+        </Link>
+      </div>
+
     </div>
   );
 }
+
+
